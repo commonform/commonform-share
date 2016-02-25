77662a7ebf74adee6a6b222f7c09da80b367590e
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-24 20:21:15 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/* Copyright 2016 Kyle E. Mitchell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = share
+
+var https = require('https')
+
+function share(form, callback) {
+  https.request(
+    { method: 'POST',
+      host: 'api.commonform.org',
+      path: '/forms' },
+    function(response) {
+      if (response.statusCode === 201) {
+        callback(null, response.headers.location) }
+      else {
+        var buffers = [ ]
+        response
+          .on('data', function(buffer) {
+            buffers.push(buffer) })
+          .on('end', function() {
+            var message = Buffer.concat(buffers).toString()
+            var error = new Error(message)
+            error.statusCode = response.statusCode
+            callback(error) }) } })
+  .end(JSON.stringify(form)) }
